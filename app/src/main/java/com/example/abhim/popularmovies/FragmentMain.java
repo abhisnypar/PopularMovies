3dc3a5ae6f28988f1d528a74017f8ba60470daa6
Abhisnypar <abhi.pidugu@gmail.com>
2016-07-02 18:43:01 -0700

    Crating a new project

---
+++ app/src/main/java/com/example/abhim/popularmovies/FragmentMain.java
@@ -0,0 +1,23 @@
+package com.example.abhim.popularmovies;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by abhim on 7/2/2016.
+ */
+public class FragmentMain extends Fragment {
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.mainfragment,container,false);
+            return rootView;
+        }
+
+    
+}
---
+++ app/src/main/java/com/example/abhim/popularmovies/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.abhim.popularmovies;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState ==null)
+            getSupportFragmentManager().beginTransaction()
+            .add(R.id.main_activity_id,new FragmentMain()).commit();
+    }
+
+
+}
